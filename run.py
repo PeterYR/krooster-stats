4414effe74419c31623ea0a7e30e086fed7ac250
PeterYR <ziyuzhang01@gmail.com>
2024-08-21 18:52:10 -0500

    output CSV to single file

--- run.py
+++ run.py
@@ -21,8 +21,8 @@ for op_name, data in kp.operators_json.items():
     op_rarities[rarity].add(op_name)
 
 
-def write_to_csv(counts: dict[str, dict[str, int]], filename: str, n_users=-1):
-    """Write results to given filename"""
+def format_csv_rows(counts: dict[str, dict[str, int]], n_users=-1, rarity=-1):
+    """Add fields for final CSV output"""
 
     # add operator ID and name to each row
     results: list[dict[str, int]] = []
@@ -31,17 +31,13 @@ def write_to_csv(counts: dict[str, dict[str, int]], filename: str, n_users=-1):
         csv_row["operator_id"] = op_id
         csv_row["operator_name"] = kp.operators_json[op_id]["name"]
         csv_row["n_users"] = n_users
+        csv_row["rarity"] = rarity
         results.append(csv_row)
 
     # sort rows
     results.sort(key=lambda x: x["operator_name"])
 
-    # write to CSV
-    columns = ["n_users", "operator_name"] + kp.COUNTED_FIELDS + ["operator_id"]
-    with open(filename, "w", newline="", encoding="utf-8") as fp:
-        writer = csv.DictWriter(fp, fieldnames=columns)
-        writer.writeheader()
-        writer.writerows(results)
+    return results
 
 
 def main(argv):
@@ -60,6 +56,7 @@ def main(argv):
     Path("./output").mkdir(exist_ok=True)
 
     # rarity-specific stats
+    csv_rows: list[dict[str, int]] = []
     for rarity, user_list in user_lists.items():
         if not user_list:  # list is empty
             continue
@@ -75,15 +72,38 @@ def main(argv):
 
         results = kp.count(rosters, accepted_ops=valid_ops, logging=True)
 
-        out_path = f"output/{rarity}.csv"
-        write_to_csv(results, out_path, n_users=len(rosters))
-        print(f"Wrote to {out_path}")
+        # out_path = f"output/{rarity}.csv"
+        csv_rows += format_csv_rows(results, n_users=len(rosters), rarity=rarity)
+
+    # csv output
+    columns = ["rarity", "n_users", "operator_name", "operator_id"] + kp.COUNTED_FIELDS
+    out_path = "output/output.csv"
+    with open(out_path, "w", newline="", encoding="utf-8") as fp:
+        writer = csv.DictWriter(fp, fieldnames=columns)
+        writer.writeheader()
+        writer.writerows(csv_rows)
+    print(f"Wrote to {out_path}")
 
     # existence flags
     flags = ef.generate_all_flags(kp.operators_json, include_cn=not EN_ONLY)
     out_path = "output/flags.csv"
     ef.write_to_csv(flags, out_path)
     print(f"Wrote to {out_path}")
+
+    # overall stats
+    valid_usernames = {n for n in usernames if roster_map.get(uuid_map.get(n))}
+    rows = [row for _, row in df.iterrows() if row["username"] in valid_usernames]
+
+    source_counts = {}
+    for row in rows:
+        source = row["source"]
+        source_counts[source] = source_counts.get(source, 0) + 1
+
+    print("\n### OVERALL STATS ###")
+    print("Source counts:")
+    print(f'  Discord: {source_counts.get("Discord", 0) / len(rows) * 100:.2f}%')
+    print(f'  Reddit: {source_counts.get("Reddit", 0) / len(rows) * 100:.2f}%')
+    print(f'  YouTube: {source_counts.get("YouTube", 0 ) / len(rows) * 100:.2f}%')
 
 
 if __name__ == "__main__":
